11cb31233f1d0fc21b42ff2d3eff85d780affac2
tomokiGymAcA <143249721+tomokiGymAcA@users.noreply.github.com>
2023-08-26 16:24:09 +0900

    Delete config directory

--- config/application.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 8000,
-    security: {
-        SESSION_SECRET: 'YOUR-SESSION-SECRET-STRING'
-    },
-};
--- config/mysql.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    HOST: "^127.0.0.1",
-    PORT: "^3306",
-    USERNAME: "tomoki",
-    PASSWORD: "gymsohgoh",
-    DATABASE: "sohgoh_job",
-}
