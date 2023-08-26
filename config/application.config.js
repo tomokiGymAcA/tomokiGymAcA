eacc6107d06d43bf2b0ea3a3706d4e92693ed84d
tomokiGymAcA <143249721+tomokiGymAcA@users.noreply.github.com>
2023-08-26 15:59:18 +0900

    Add files via upload

---
+++ config/application.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    security: {
+        SESSION_SECRET: 'YOUR-SESSION-SECRET-STRING'
+    },
+};
---
+++ config/mysql.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "^127.0.0.1",
+    PORT: "^3306",
+    USERNAME: "tomoki",
+    PASSWORD: "gymsohgoh",
+    DATABASE: "sohgoh_job",
+}
